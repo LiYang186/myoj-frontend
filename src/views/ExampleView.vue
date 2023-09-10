1a8ffd3fe0bc127d2a0132c79acb204751717e54
LiYang186 <1443510186@qq.com>
2023-09-10 22:02:46 +0800

    feat:代码编辑器，md编辑器

---
+++ src/views/ExampleView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="exampleView">
+    <MdEditor :value="mdValue" :handle-change="onMdChange" />
+    <CodeEditor :value="codeValue" :handle-change="onCodeChange" />
+
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineComponent, ref } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+import MdEditor from "@/components/MdEditor.vue";
+import CodeEditor from "@/components/CodeEditor.vue"; // @ is an alias to /src
+
+const codeValue = ref();
+const mdValue = ref();
+const onMdChange = (v: string) => {
+  mdValue.value = v;
+};
+
+const onCodeChange = (v: string) => {
+  codeValue.value = v;
+};
+</script>
